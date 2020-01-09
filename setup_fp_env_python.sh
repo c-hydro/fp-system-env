c915d03960aeaf50de29370de7ed4a0a8c866e22
c-hydro <fabio.delogu@cimafoundation.org>
2020-01-09 12:01:03 +0100

    commit_20200109_11:01

--- setup_fp_env_python.sh
+++ setup_fp_env_python.sh
@@ -3,13 +3,23 @@
 #-----------------------------------------------------------------------------------------
 # Script information
 script_name='FP ENVIRONMENT - PYTHON3 LIBRARIES'
-script_version="1.0.10"
-script_date='2019/10/31'
+script_version="1.1.0"
+script_date='2020/01/09'
 
 fileref_miniconda='https://repo.continuum.io/miniconda/Miniconda3-4.5.11-Linux-x86_64.sh'
 
-fp_folder_libs=/home/fabio/Documents/Work_Area/Code_Development/Library/virtualenv_python3
-fp_env_libs='virtualenv_python3'
+# Get folder root path
+if [ $# -eq 0 ]; then
+    fp_folder_root=$HOME/fp_libs_python3
+else
+	fp_folder_root=$1
+	if [ ! -d "$fp_folder_root" ]; then
+		mkdir -p $fp_folder_root
+	fi
+fi
+
+fp_folder_libs=$fp_folder_root
+fp_env_libs='fp_env_python3'
 
 # multilines comment: if [ 1 -eq 0 ]; then ... fi
 #-----------------------------------------------------------------------------------------
@@ -39,7 +49,7 @@ echo " ====> INSTALL PYTHON ENVIRONMENT ... DONE!"
 # ----------------------------------------------------------------------------------------
 # Install python libraries
 echo " ====> INSTALL PYTHON LIBRARIES ... "
-conda create -n $fp_env_libs -c conda-forge numpy scipy pandas matplotlib rasterio geopandas netCDF4 pynetcf pyflakes statsmodels cython h5py jupyter pybufr-ecmwf pykdtree pygrib pyresample cartopy basemap basemap-data-hires proj4 progressbar2 xarray pygeobase execnet memon dask pip python=3
+conda create -n $fp_env_libs -c conda-forge numpy scipy pandas matplotlib rasterio geopandas netCDF4 pyflakes statsmodels cython h5py jupyter pybufr-ecmwf pykdtree pygrib pyresample cartopy basemap basemap-data-hires proj4 progressbar2 xarray pygeobase dask pip python=3
 source activate $fp_env_libs
 pip install pygeogrids
 pip install h5netcdf
@@ -47,8 +57,10 @@ pip install ascat
 pip install pytesmo
 pip install repurpose
 pip install jupyter
+pip install pynetcf
 pip install JPype1-py3
 conda install -c conda-forge rise
+conda install -c conda-forge nbconvert
 echo " ====> INSTALL PYTHON LIBRARIES ... DONE!"
 # ----------------------------------------------------------------------------------------
 
